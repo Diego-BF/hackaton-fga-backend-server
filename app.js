8b588444609dc9ac88f1f4d796cfe5469baf2637
diego-bf <diegobmfranca@gmail.com>
2020-05-18 17:42:23 -0300

    Adding parsers to express

--- app.js
+++ app.js
@@ -4,5 +4,15 @@ import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => res.send('Hello world, how are you?'));
+app.put('/users/:userId', (req, res) => {
+  return res.send(`PUT method in user with id ${req.params.userId}`);
+});
+app.delete('/users/:userId', (req, res) => {
+  return res.send(`DELETE method in user with id ${req.params.userId}`);
+});
+
 app.listen(PORT, () => console.log(`app running at port ${PORT}`));
