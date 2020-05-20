b9ac6cddd7f35dd470e44e9af0ca0e0f85acf9f2
diego-bf <diegobmfranca@gmail.com>
2020-05-20 03:03:40 -0300

    Adding a model to users

---
+++ models/user.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    address: {
+      street: {
+        type: String,
+        required: true,
+      },
+      city: {
+        type: String,
+        required: true,
+      },
+      zip: {
+        type: String,
+        required: true,
+      },
+      isConsumer: {
+        type: Boolean,
+        required: true,
+        default: true,
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
