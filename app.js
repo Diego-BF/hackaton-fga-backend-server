454f658abaf3fd7b9af3ddb18a9ad3b3d6010076
diego-bf <diegobmfranca@gmail.com>
2020-05-19 21:41:02 -0300

    Cleaning app.js
    - remove old routes
    - change a context property name

--- app.js
+++ app.js
@@ -1,33 +1,23 @@
-import 'dotenv/config.js';
-import express from 'express';
-import routes from './routes/index.js';
-import models from './models/index.js';
+import "dotenv/config.js";
+import express from "express";
+import routes from "./routes";
+import models from "./models";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.use('/', (req, res, next) => {
+app.use("/", (req, res, next) => {
   req.context = {
     models: models,
-    me: models.users[1]
-  }
+    user: models.users[1],
+  };
   next();
 });
 
-app.use('/users', routes.users);
-app.use('/messages', routes.messages);
-app.use('/session', routes.session);
-
-
-
-
-app.put('/users/:userId', (req, res) => {
-  return res.send(`PUT method in user with id ${req.params.userId}`);
-});
-app.delete('/users/:userId', (req, res) => {
-  return res.send(`DELETE method in user with id ${req.params.userId}`);
-});
+app.use("/users", routes.users);
+app.use("/messages", routes.messages);
+app.use("/session", routes.session);
 
 app.listen(PORT, () => console.log(`app running at port ${PORT}`));
