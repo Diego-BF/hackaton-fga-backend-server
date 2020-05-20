2e2f14b8a3c34e122e57f90a1fc64a898e940cc7
Gabriel Williams <gabriel.williams44@gmail.com>
2020-05-20 03:21:19 -0300

    Added properties to ORM objects

--- models/city.js
+++ models/city.js
@@ -3,6 +3,11 @@ const Schema = mongoose.Schema;
 
 var CitySchema = new Schema ({
     _id: Schema.Types.ObjectId,
+    name: {
+        type: String,
+        unique: true,
+        required: true
+    }
     },
     { timestamps: true }    // data e horario
 );
--- models/product.js
+++ models/product.js
@@ -3,6 +3,21 @@ const Schema = mongoose.Schema;
 
 var ProductSchema = new Schema ({
     _id: Schema.Types.ObjectId,
+        name: {
+            type: String,
+            unique: true,
+            required: true
+        },
+        image: {
+            type: String,
+            unique: true,
+            required: false
+        },
+        synonyms: {
+            type: [String],
+            unique: true,
+            required: false
+        }
     },
     { timestamps: true }    // data e horario
 );
